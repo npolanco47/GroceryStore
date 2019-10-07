5f41dcc83756e78e2794856113486e2b16d4ae14
npolanco47 <npolanco@mail.csuchico.edu>
2019-10-07 16:21:13 -0700

    few changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//Nixon Polanco //Author
+
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +9,17 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  char answer;
 
+do 
+{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-
+  cin>>answer;
+}
+while(answer != 'Q');
   return 0;
 }
